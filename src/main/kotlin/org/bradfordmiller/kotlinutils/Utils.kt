172d3182dea9fb8415fa1a231b96265fe69528c3
Brad Miller <brad.miller@us.imshealth.com>
2020-02-26 17:17:33 -0500

    Initial commit

---
+++ src/main/kotlin/org/bradfordmiller/kotlinutils/Utils.kt
@@ -0,0 +1,15 @@
+package org.bradfordmiller.kotlinutils
+/**
+ * represents Either construct with [Left] and [Right] values
+ */
+sealed class Either<A, B>
+/**
+ *  @param A the [left] value in an [Either] construct
+ *  @param B the right value in an [Either] construct
+ */
+data class Left<A, B>(val left: A) : Either<A, B>()
+/**
+ *  @param A the left value in an [Either] construct
+ *  @param B the [right] value in an [Either] construct
+ */
+data class Right<A, B>(val right: B) : Either<A, B>()
